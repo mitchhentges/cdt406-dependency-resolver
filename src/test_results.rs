e2a38f9c7b82f14fc31adf411a09e4542a9eafa4
Mitchell Hentges <mitchhentges@protonmail.com>
2016-05-04 19:01:48 +0200

    Can use mock string

--- src/test_results.rs
+++ src/test_results.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{BufReader, BufRead};
+use std::io::{BufReader, BufRead, Read};
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Test {
@@ -41,32 +41,48 @@ impl<'a> CsvTestSource<'a> {
 impl<'a> TestSource for CsvTestSource<'a> {
     fn read_tests(&self) -> Result<TestResults, String> {
         let file = try!(File::open(self.filename).map_err(|e| e.to_string()));
-        let reader = BufReader::new(file);
+        let result = parse(BufReader::new(file));
+        Ok(TestResults::new(result.unwrap()))
+    }
+}
 
-        let mut all_results: Vec<Test> = Vec::new();
-        for line in reader.lines() {
-            if line.is_err() {
-                println!("Error occurred while reading file, using input up to here");
-                println!("\t{}", line.unwrap_err().to_string());
-                break;
-            }
-
-            let line = line.unwrap();
-            let test_executions = line.matches(',').count(); // First chunk is the test name
-
-            if test_executions < 1 { // There's not even a single test
-                println!("No test executions for test {}", line);
-                continue;
-            }
-
-            let test_name = line[0..line.find(',').unwrap()].to_owned();
-            let results: Vec<bool> = line.split(',')
-                .skip(1)
-                .map(|result| result == "0")
-                .collect();
-            let test = Test::new(test_name, results);
-            all_results.push(test);
+pub fn parse<T: Read>(reader: BufReader<T>) -> Result<Vec<Test>, String> {
+    let mut all_results: Vec<Test> = Vec::new();
+    for line in reader.lines() {
+        if line.is_err() {
+            println!("Error occurred while reading file, using input up to here");
+            println!("\t{}", line.unwrap_err().to_string());
+            break;
         }
-        Ok(TestResults::new(all_results))
+
+        let line = line.unwrap();
+        let test_executions = line.matches(',').count(); // First chunk is the test name
+
+        if test_executions < 1 { // There's not even a single test
+            println!("No test executions for test {}", line);
+            continue;
+        }
+
+        let test_name = line[0..line.find(',').unwrap()].to_owned();
+        let results: Vec<bool> = line.split(',')
+        .skip(1)
+        .map(|result| result == "0")
+        .collect();
+        let test = Test::new(test_name, results);
+        all_results.push(test);
+    }
+    Ok(all_results)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::{BufReader, BufRead, Read};
+
+    #[test]
+    fn bork() {
+        let reader = BufReader::new("A,0,1".as_bytes());
+        let result = parse(reader);
+        println!("{:?}", result);
     }
 }
