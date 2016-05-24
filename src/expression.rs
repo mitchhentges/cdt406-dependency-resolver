e5ae81b3b73ff0706471ffab880468d37702c0ff
Mitchell Hentges <mitchhentges@protonmail.com>
2016-05-24 15:55:56 +0200

    Fix mixed vector typing in expression

--- src/expression.rs
+++ src/expression.rs
@@ -88,7 +88,7 @@ mod tests {
     }
 
     fn evaluate(expression: &Expression, results: &Vec<bool>) {
-        let mut mapping = Vec::<i32>::with_capacity(results.len());
+        let mut mapping = Vec::<usize>::with_capacity(results.len());
         mapping.extend(0..results.len());
         return expression.evaluate(results, &mapping);
     }
